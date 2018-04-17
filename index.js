c005eb15594be2cb9c6ec1ff88f73e87e125f3a2
David <dzikel@carthage.edu>
2018-04-17 15:21:50 -0500

    tweak formatting

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ window.onload = function()
     });
 
 
-    /*var newNode = SpanningTreeNode("A");
+    /*
+    var newNode = SpanningTreeNode("A");
     var pushNode = SpanningTreeNode("B");
     var pushNode2 = SpanningTreeNode("C");
 
@@ -20,7 +21,8 @@ window.onload = function()
     for (var i = 0; i < newNode.listOfLinkedNodes.length; i++)
     {
         alert(newNode.listOfLinkedNodes[i].rootIs);
-    }*/
+    }
+    */
 }
 
 function setup() 
