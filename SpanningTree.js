6da92ea3871b2c1cced91f0221e5d3ab6d9e1eae
mdelreal2 <mdelreal2@carthage.edu>
2018-04-17 14:43:14 -0500

    Starting of the logic additions

---
+++ SpanningTree.js
@@ -0,0 +1,15 @@
+//The Spanning Tree Protocol (STP) is a network protocol that 
+//builds a loop-free logical topology for Ethernet networks. 
+//The basic function of STP is to prevent bridge loops 
+//and the broadcast radiation that results from them.
+function SpanningTreeNode(label)
+{
+    var node = {
+        rootIs:label, 
+        hopsToRoot:0, 
+        sender:label,
+        listOfLinkedNodes:[]
+    };
+    
+    return node;
+}
--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+//will be used to add event listeners to any DOMs that need to be interacted with
 window.onload = function()
 {
-    
+    //setup all element listeners
+    var nodeNumberSelect = document.getElementById("_node_number_select");
+    nodeNumberSelect.addEventListener("change", function(){
+        //call logic to reset canvas and redisplay a network of X number of Nodes
+        //apply algorithm to these nodes
+        alert("adf");
+    });
+
+
+    /*var newNode = SpanningTreeNode("A");
+    var pushNode = SpanningTreeNode("B");
+    var pushNode2 = SpanningTreeNode("C");
+
+    newNode.listOfLinkedNodes.push(pushNode);
+    newNode.listOfLinkedNodes.push(pushNode2);
+
+    for (var i = 0; i < newNode.listOfLinkedNodes.length; i++)
+    {
+        alert(newNode.listOfLinkedNodes[i].rootIs);
+    }*/
 }
 
 function setup() 
@@ -10,13 +30,13 @@ function setup()
 
 function draw() 
 {
-    if (mouseIsPressed)
-    {
-        fill(0);
-    }
-    else
-    {
-        fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
+    // if (mouseIsPressed)
+    // {
+    //     fill(0);
+    // }
+    // else
+    // {
+    //     fill(255);
+    // }
+    // ellipse(mouseX, mouseY, 80, 80);
 }
