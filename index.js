786e3593927a316addad8894a09ab63a078aab24
Dimitrije <dkastratovic@carthage.edu>
2018-04-19 15:13:29 -0500

    Added clickable circle

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var numNodes = 0;
+var isOverCircle;
+var backgroundColor;
 var listOfNodes = [];
-
 
 //will be used to add event listeners to any DOMs that need to be interacted with
 window.onload = function()
@@ -16,7 +17,6 @@ window.onload = function()
     });
 
     initializeNodes();
-
 
     /*
     var newNode = SpanningTreeNode("A");
@@ -36,18 +36,44 @@ window.onload = function()
 function setup() 
 {
     createCanvas(640, 480);
+    backgroundColor = color(255, 255, 255);
 }
 
 function draw() 
 {
-    // if (mouseIsPressed)
-    // {
-    //     fill(0);
-    // }
-    // else
-    // {
-    //     fill(255);
-    // }
-    // ellipse(mouseX, mouseY, 80, 80);
+    background(backgroundColor);
+    // get distance between mouse and circle
+    var distance = dist(mouseX, mouseY, 200, 200); 
+     
+    // if the distance is less than the circle's radius
+    if(distance < 50)
+    {
+     isOverCircle = true;
+    } else {
+       isOverCircle = false;
+    }
+     
+    //draw a circle
+    ellipseMode(CENTER);
+    stroke(0);
+    strokeWeight(5);
+
+    if(isOverCircle == true)
+    {
+      fill(100);
+      cursor(HAND);
+    } else {
+     fill(200); 
+     cursor(ARROW); 
+    }
+    ellipse(200, 200, 100, 100);
 }
-//hello
+
+function mousePressed()
+{
+  if(isOverCircle == true)
+  {
+    backgroundColor = color(random(255), random(255), random(255));
+  }
+}
+
