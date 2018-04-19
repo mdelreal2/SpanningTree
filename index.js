84942fd17ff203d2cc8e67bd0862a4ff694454ea
David <dzikel@carthage.edu>
2018-04-19 15:22:28 -0500

    oops

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ window.onload = function()
     nodeNumberSelect.addEventListener("change", function(){
         //call logic to reset canvas and redisplay a network of X number of Nodes
         //apply algorithm to these nodes
-        numNodes = nodeNumberSelect.value;
+        if (nodeNumberSelect.value != 0)
+        {
+            numNodes = nodeNumberSelect.value;
+        }
     });
 
     initializeNodes();
